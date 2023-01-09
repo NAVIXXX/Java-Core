da0f462f188cfd1a0e193d38e618aa9288cace9e
NAVIXXX <jose.aimituma@tecsup.edu.pe>
2023-01-09 01:19:15 -0500

    commit first

---
+++ src/core/tiposdedatos.java
@@ -0,0 +1,14 @@
+package core;
+
+public class tiposdedatos {
+    /**
+     * Tipos de datos primitivos  y complejos
+     * Dato primitivo
+     *
+     */
+    public char cLetra = 'a';
+    private String sSaludo = "Hola";
+    public int iNumero = 4;
+    public boolean bVerdad = true;
+
+}
